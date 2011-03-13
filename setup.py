173e2976b39a9fd5a5d857e3ea4021abeb5f802c
Josh Holland <jrh@joshh.co.uk>
2011-03-13 14:40:19 +0000

    Create setup.py and add/modify other files

---
+++ setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+        name='dutch-bits',
+        version='0.1',
+        description='Homebrew Python toolkit. Use at your own risk :)',
+        author='Josh Holland',
+        author_email='jrh@joshh.co.uk',
+        url='http://github.com/jshholland/pytools.git',
+        classifiers = [
+            'Development Status :: 3 - Alpha',
+            'License :: OSI Approved :: BSD License',
+            'Operating System :: OS Independent',
+            'Topic :: Scientific/Engineering :: Mathematics',
+            'Topic :: Utilities',
+        ],
+        py_modules = ['inflist']
+ )
